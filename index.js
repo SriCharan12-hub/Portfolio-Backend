cec360c2cd3b1ee14f02cb96482b4283bc2a7486
sricharan12-hub <sric6618@gmail.com>
2026-03-28 06:16:55 +0530

    smntp changes

--- index.js
+++ index.js
@@ -14,7 +14,10 @@ app.use(express.json());
 
 // Nodemailer Configuration
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  family: 4,
   auth: {
     user: process.env.APP_EMAIL,
     pass: process.env.APP_PASSWORD,
